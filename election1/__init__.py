93f45d110da928e4cd19b75171e541231523d028
mpo6767 <49372957+mpo6767@users.noreply.github.com>
2024-03-02 21:39:02 -0600

    init sqlite hardcoded insertion of admin roles

--- election1/__init__.py
+++ election1/__init__.py
@@ -3,8 +3,9 @@ import logging.config
 from flask import Flask
 from .models import User
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy_utils import database_exists
 from election1.config import Config
+from sqlalchemy import func, exc
 
 logging.config.fileConfig('logging.conf')
 
@@ -72,11 +73,12 @@ def config_application(app):
     app.config["RECAPTCHA_PRIVATE_KEY"] = os.getenv('RECAPTCHA_KEY', None)
 
     # SQLAlchemy configuration
-    db_user = os.environ.get('DB_USER')
-    db_pass = os.environ.get('DB_PASS')
+    # db_user = os.environ.get('DB_USER')
+    # db_pass = os.environ.get('DB_PASS')
+    # app.config["SQLALCHEMY_DATABASE_URI"] =('mysql+pymysql://' + str(db_user) + ':' +
+    #                            str(db_pass) + '@localhost/school_db')
 
-    app.config["SQLALCHEMY_DATABASE_URI"] =('mysql+pymysql://' + str(db_user) + ':' +
-                               str(db_pass) + '@localhost/school_db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///election.db'
 
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
@@ -108,14 +110,42 @@ def config_extention(app):
     from .extensions import bootstrap
     from .extensions import csrf
 
-    db.init_app(app)
+    if not os.path.exists("instance/election.db"):
+        logger.info("database is  not here")
+        db.init_app(app)
+        db_name = "election.db"
+        with app.app_context():
+            # if database_exists('sqlite:///instance/' + db_name):
+            if database_exists(app.config['SQLALCHEMY_DATABASE_URI']):
+                logger.info(db_name + " already exists")
+            else:
+                logger.info(db_name + " does not exist, will create " + db_name)
+                try:
+                    db.create_all()
+                    id_admin_role = 1
+                    admin_role_name = "Super Admin"
+                    new_admin_role = models.Admin_roles(id_admin_role=id_admin_role,
+                                                        admin_role_name=admin_role_name)
+                    db.session.add(new_admin_role)
+                    id_admin_role = 2
+                    admin_role_name = "Election Admin"
+                    new_admin_role = models.Admin_roles(id_admin_role=id_admin_role,
+                                                        admin_role_name=admin_role_name)
+                    db.session.add(new_admin_role)
+                    db.session.commit()
+
+                except exc.SQLAlchemyError as sqlalchemyerror:
+                    logger.info("got the following SQLAlchemyError: " + str(sqlalchemyerror))
+                except Exception as exception:
+                    logger.info("got the following Exception: " + str(exception))
+                finally:
+                    logger.info("db.create_all() in __init__.py was successfull - no exceptions were raised")
+
+
     login_manager.init_app(app)
     config_manager(login_manager)
     csrf.init_app(app)
 
-    with app.app_context():
-        admin_check = User.query.first()
-        print(str(admin_check))
 
 
 def config_manager(manager):
@@ -131,6 +161,3 @@ def config_manager(manager):
     @manager.user_loader
     def user_loader(id):
         return User.query.get_or_404(id)
-
-
-
