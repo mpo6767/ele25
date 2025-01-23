a7450755688afb2a66f54095fa1fe56fbc7125b2
Michael OConnor <michael@cpo2llc.com>
2025-01-23 13:00:02 -0600

    modified run to auto compile

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
+import os
 from flask import Flask
-
 from election1 import create_app
 
 app: Flask = create_app()
 
 if __name__ == '__main__':
-
-    app.run(debug=False)
+    os.environ['FLASK_ENV'] = 'development'
+    os.environ['FLASK_DEBUG'] = '1'
+    app.run(debug=True)
